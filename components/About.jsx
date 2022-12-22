dc5c90363dc0c24b6cef5a12cbac583e547d3371
Jeff Derbes <derbes.jeffrey@gmail.com>
2022-12-22 14:18:33 -0600

    projects section

--- components/About.jsx
+++ components/About.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import me from '../public/assets/me.jpeg';
 
 
-const About = () => {
+function About() {
     return (
         <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
             <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,43 +1,49 @@
-import React from 'react';
-import {FaGithub, FaLinkedin} from "react-icons/fa";
-import {AiOutlineMail} from "react-icons/ai";
-import {BsPersonLinesFill} from "react-icons/bs";
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsPersonLinesFill } from "react-icons/bs";
 
-const Main = (props) => {
-    return (
-        <div className="w-full h-screen text-center">
-            <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-                <div>
-                    <p className="uppercase text-sm tracking-widest text-gray-200">Let's build something together</p>
-                    <h1 className="py-4">Hi, I'm <span className="text-[#40dbfc]">Jeff</span></h1>
-                    <h2 className="py-2">A Full-stack Developer</h2>
-                    <p className="py-4 max-w-[70%] m-auto">I'm a full-stack web developer specializing in building
-                        elegant web experiences. I build responsive full-stack apps and love learning and implementing
-                        new technologies </p>
-                    <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-                        <div
-                            className="p-3 cursor-pointer hover:text-black hover:bg-[#40dbfc]
-                             hover:rounded-full transition-all duration-300">
-                            <FaLinkedin size={30}/>
-                        </div>
-                        <div
-                            className="p-3 cursor-pointer hover:text-black hover:bg-[#40dbfc] hover:rounded-full transition-all duration-300">
-                            <FaGithub size={30}/>
-                        </div>
-                        <div
-                            className="p-3 cursor-pointer hover:text-black hover:bg-[#40dbfc]
-                             hover:rounded-full transition-all duration-300">
-                            <AiOutlineMail size={30}/>
-                        </div>
-                        <div
-                            className="p-3 cursor-pointer hover:text-black hover:bg-[#40dbfc] hover:rounded-full transition-all duration-300">
-                            <BsPersonLinesFill size={30}/>
-                        </div>
-                    </div>
-                </div>
+function Main(props) {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-200">
+            Let's build something together
+          </p>
+          <h1 className="py-4">
+            Hi, I'm <span className="text-[#40dbfc]">Jeff</span>
+          </h1>
+          <h2 className="py-2">A Full-stack Developer</h2>
+          <p className="py-4 max-w-[70%] m-auto">
+            I'm a full-stack web developer specializing in building elegant web
+            experiences. I build responsive full-stack apps and love learning
+            and implementing new technologies{" "}
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div
+              className="p-3 cursor-pointer hover:text-black hover:bg-[#40dbfc]
+                             hover:rounded-full transition-all duration-300"
+            >
+              <FaLinkedin size={30} />
             </div>
+            <div className="p-3 cursor-pointer hover:text-black hover:bg-[#40dbfc] hover:rounded-full transition-all duration-300">
+              <FaGithub size={30} />
+            </div>
+            <div
+              className="p-3 cursor-pointer hover:text-black hover:bg-[#40dbfc]
+                             hover:rounded-full transition-all duration-300"
+            >
+              <AiOutlineMail size={30} />
+            </div>
+            <div className="p-3 cursor-pointer hover:text-black hover:bg-[#40dbfc] hover:rounded-full transition-all duration-300">
+              <BsPersonLinesFill size={30} />
+            </div>
+          </div>
         </div>
-    )
-};
+      </div>
+    </div>
+  );
+}
 
 export default Main;
