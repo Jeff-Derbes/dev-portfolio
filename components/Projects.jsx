5db3a2af13954afcb9a65a581d997402f9cdcc8c
Jeff Derbes <derbes.jeffrey@gmail.com>
2022-12-23 14:28:46 -0600

    added mypub project and made fix to project item component

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import Image from "next/image";
 import lavenderArt from "../public/assets/projects/lavender-art.png";
+import mypub from "../public/assets/projects/mypub.png";
 import ProjectItem from "./ProjectItem";
 
 function Projects(props) {
   return (
-    <div id="projects" className="w-full">
-      <div className="max-w-[1240px] mx-auto px-2 py-16">
-        <p className="text-xl tracking-widest uppercase text-[#40dbfc]">
-          Projects
-        </p>
-        <h2 className="py-4">What I&apos;ve Built</h2>
-        <div className="grid md:grid-cols-2 gap-8">
-          <ProjectItem
-            title="Lavender Art"
-            backgroundImg={lavenderArt}
-            projectUrl="/lavender-art"
-          />
+      <div id="projects" className="w-full">
+        <div className="max-w-[1240px] mx-auto px-2 py-16">
+          <p className="text-xl tracking-widest uppercase text-[#40dbfc]">
+            Projects
+          </p>
+          <h2 className="py-4">What I&apos;ve Built</h2>
+          <div className="grid md:grid-cols-2 gap-8">
+            <ProjectItem
+                title="Lavender Art"
+                backgroundImg={lavenderArt}
+                projectUrl="/lavender-art"
+            />
+
+            <ProjectItem
+                title="MyPub"
+                backgroundImg={mypub}
+                projectUrl="/mypub"
+            />
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
---
+++ pages/mypub.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Image from "next/image";
+import mypub from "../public/assets/projects/mypub.png";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
+
+function lavenderArt(props) {
+  return (
+    <div className="w-full">
+      <div className="w-screen h-[40vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[40vh] bg-black/70 z-10" />
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={mypub}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">Lavender Art</h2>
+          <h3>React JS / CSS / Firebase</h3>
+        </div>
+      </div>
+
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+        <div className="col-span-4">
+          <p>Project</p>
+          <h2 className="py-4">Overview</h2>
+          <p className="pb-10 leading-relaxed max-w-[80%]">
+            This website was my first relatively large project and my bootcamp
+            capstone. It&apos;s a cocktail recipe search application that allows
+            users to log in, discover, and save new cocktail recipes to try out.
+            It uses React JS on the frontend and Firebase for data, hosting, and
+            authentication.
+          </p>
+          <a
+            href="https://www.lavenderart.co/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4  border hover:text-slate-900 hover:bg-slate-100 hover:border-slate-900 transition-all duration-300">
+              Visit Site
+            </button>
+          </a>
+        </div>
+        <div className="col-span-4 md:col-span-1 border rounded-xl py-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1 px-4">
+              <p className="py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> React
+              </p>
+              <p className="py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> CSS
+              </p>
+              <p className="py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> JavaScript
+              </p>
+              <p className="py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Firebase
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href="/#projects">
+          <p className="underline cursor-pointer">Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default lavenderArt;
