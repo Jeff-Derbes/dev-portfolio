1b7c6e33683b00b361f468a09e39019dd7814916
Jeff Derbes <derbes.jeffrey@gmail.com>
2022-12-23 10:30:15 -0600

    main section updates and added sine wave svg to home

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -69,7 +69,7 @@ function Contact(props) {
           <div className="col-span-3 w-full h-auto border rounded-xl lg:p-4">
             <div className="p-4">
               <form
-                action="https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060"
+                action="https://getform.io/f/6ecedcf4-1237-4ac4-b74a-8fa5190498c4"
                 method="POST"
                 encType="multipart/form-data"
               >
